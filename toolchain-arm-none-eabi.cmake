cf944dae3d132f75564546b8924c716ada95271d
Charles Korn <me@charleskorn.com>
2016-04-24 16:38:04 +1000

    Specify floating point unit type.

--- toolchain-arm-none-eabi.cmake
+++ toolchain-arm-none-eabi.cmake
@@ -13,7 +13,7 @@ set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
 find_program(ARM_SIZE_EXECUTABLE arm-none-eabi-size)
 
-set(shared_options "-mlittle-endian -mthumb -mcpu=cortex-m4 -mthumb-interwork -mfloat-abi=hard -ffreestanding -ffunction-sections -fdata-sections -nostdlib")
+set(shared_options "-mlittle-endian -mthumb -mcpu=cortex-m4 -mthumb-interwork -mfloat-abi=hard -mfpu=fpv4-sp-d16 -ffreestanding -ffunction-sections -fdata-sections")
 set(CMAKE_C_FLAGS_INIT "${shared_options}" CACHE INTERNAL "Initial options for C compiler.")
 set(CMAKE_CXX_FLAGS_INIT "${shared_options}" CACHE INTERNAL "Initial options for C++ compiler.")
 set(CMAKE_EXE_LINKER_FLAGS_INIT "-Wl,--gc-sections" CACHE INTERNAL "Initial options for executable linker.")
