f5b9d32897a92b3bfb7a484b2a2d3d40e1322b90
Charles Korn <me@charleskorn.com>
2016-05-08 14:38:37 +1000

    Fail the test run if the tests fail on the target.

--- tools/run-tests-on-target.sh
+++ tools/run-tests-on-target.sh
@@ -8,15 +8,19 @@ OPENOCD_CONFIG=$4
 OPENOCD_PORT=3333
 
 white=$(tput setaf 7)
+green=$(tput setaf 2)
+red=$(tput setaf 1)
 reset=$(tput sgr0)
 
-echo "${white}Running $ELF_FILE using GDB ($ARM_GDB_EXECUTABLE) and OpenOCD ($OPENOCD_EXECUTABLE) with config $OPENOCD_CONFIG${reset}"
+echo "${white}Running $ELF_FILE using GDB ($ARM_GDB_EXECUTABLE) and OpenOCD ($OPENOCD_EXECUTABLE) with config ${OPENOCD_CONFIG}${reset}"
+
+TEST_OUTPUT_FILE=$(mktemp)
 
 # Unfortunately we can't use the GDB "target remote | openocd -c 'gdb_port pipe' ..." syntax
 # as that then sends all stdout to GDB - we want the board to be able to write to stdout using
 # semihosting. So we have to manage the openocd process ourselves.
 
-${OPENOCD_EXECUTABLE} -c "gdb_port ${OPENOCD_PORT}; debug_level 1" -f ${OPENOCD_CONFIG} &
+${OPENOCD_EXECUTABLE} -c "gdb_port ${OPENOCD_PORT}; debug_level 1" -f $OPENOCD_CONFIG > >(tee $TEST_OUTPUT_FILE) &
 OPENOCD_PID=$!
 
 ${ARM_GDB_EXECUTABLE} \
@@ -33,8 +37,24 @@ ${ARM_GDB_EXECUTABLE} \
         --eval-command "monitor reset init" \
         --eval-command "echo ${white}Starting tests...\n${reset}" \
         --eval-command "continue" \
-        --eval-command "echo ${white}Tests complete!\n${reset}" \
+        --eval-command "echo ${white}Test run completed.\n${reset}" \
         --eval-command "quit" \
         --batch
 
 kill $OPENOCD_PID
+
+# Wait for OpenOCD to quit, and don't display the 'terminated job' message.
+wait $OPENOCD_PID 2>/dev/null
+
+grep -xq '\x1B\[1;32mSuccess!\x1B\[0m' $TEST_OUTPUT_FILE
+SEARCH_RESULT=$? # grep returns 0 if a match is found, 1 if no match is found and something greater than 1 if an error occurs
+
+rm $TEST_OUTPUT_FILE
+
+if [[ $SEARCH_RESULT -eq 0 ]]; then
+    echo "${green}All tests passed.${reset}"
+    exit 0
+else
+    echo "${red}One or more tests failed or the test run did not complete successfully. See above for details.${reset}"
+    exit 1
+fi
